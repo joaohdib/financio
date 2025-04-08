ea2e7d7e10f0314c1652b61b0598105b95643ea3
joaohdib <joaohdib@gmail.com>
2025-04-08 18:22:29 -0300

    routes for get payments and add them, login route added as well

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -21,10 +21,10 @@ async function jwtToken() {
     return token;
 }
 
-const signUp = async (req: Request, res: Response) => {
+const signUp = async (req: Request, res: Response): Promise<void> => {
     console.log(req.body);
     if (!req.body.email || !req.body.password || !req.body.username) {
-        res.status(400).json({ error: "Senha, email e username sao obrigatorios" });
+        res.status(400).json({ error: "Password, email, and username are required" });
     }
     const cryptedPass = await hashPassword(req.body.password);
 
@@ -39,10 +39,10 @@ const signUp = async (req: Request, res: Response) => {
     res.status(200).json({ message: "User criado com sucesso!", user: user });
 }
 
-const login = async (req: Request, res: Response) => {
+const login = async (req: Request, res: Response): Promise<void> => {
     try {
         if (!req.body.email || !req.body.password) {
-            res.status(400).json({ error: "Senha, email e username sao obrigatorios" });
+            res.status(400).json({ error: "Email and password are required" });
         }
         const user = await prisma.user.findUnique({
             where: {
@@ -52,13 +52,13 @@ const login = async (req: Request, res: Response) => {
         if (user) {
             if (user?.password && await bcrypt.compare(req.body.password, user.password)) {
                 const token = await jwtToken();
-                res.status(200).json({ message: "User logado com sucesso!", token: token });
+                res.status(200).json({ message: "User successfully logged in!", token: token });
             }
             else {
-                res.status(400).json({ error: "Senha incorreta" })
+                res.status(400).json({ error: "Incorrect password" })
             }
         } else {
-            res.status(404);
+            res.status(404).json({ error: "User not found" });
         }
     } catch (err) {
         res.status(500).json({ error: "An unexpected error occurred", details: err });
